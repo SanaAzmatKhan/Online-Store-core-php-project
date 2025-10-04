ea53894d77ff1cfd74640ec4af5b596807d5e612
Sana Azmat Khan <sana.azmat@waada.pk>
2025-10-04 23:53:36 +0500

    refactor ajax operation file

--- LOOK_Online_Shop/adminPanel/AddCategory.php
+++ LOOK_Online_Shop/adminPanel/AddCategory.php
@@ -1,51 +1,37 @@
-<?php include_once("navbar.php")?>
-	
-
+<?php include_once("navbar.php") ?>
 <div id="page-wrapper">
-<div class="container">
-	<div></div>
-		<div class="form-grids row widget-shadow" data-example-id="basic-forms"> 
-						<div class="form-title">
-							<h4>Add Category :</h4>
-						</div>
-						<div class="form-body">
-						
-							
-							<form>
-														<div class="form-group">
-														<label>Enter The Category Name</label>
-														<input type="text" name="cat_name" style="width: 50%;" class="form-control" id="cat_name"/>
-														</div>
-														<button onClick="Insert();" class="btn btn-success">Insert</button>
-													
-													</form>
-							
-						</div>
+	<div class="container">
+		<div></div>
+		<div class="form-grids row widget-shadow" data-example-id="basic-forms">
+			<div class="form-title">
+				<h4>Add Category :</h4>
+			</div>
+			<div class="form-body">
+				<form>
+					<div class="form-group">
+						<label>Enter The Category Name</label>
+						<input type="text" name="cat_name" style="width: 50%;" class="form-control" id="cat_name" />
 					</div>
-	
+					<button onClick="Insert();" class="btn btn-success">Insert</button>
+				</form>
+			</div>
+		</div>
+	</div>
 </div>
-</div>
-
 <script>
-		   							function Insert(){
-										
-										var cname=$("#cat_name").val();
-										
-										$.ajax({
-											
-											type:"POST",
-											url:"AjaxOperation.php",
-											data:{catname:cname,method:'insertcat'},
-											success:function(data){
-												alert(data);
-											}
-											
-											
-										})
-										
-										
-									}
-		   
-		   
-		   					</script>
-	<?php include_once("footer.php")?>
+	function Insert() {
+		var cname = $("#cat_name").val();
+		$.ajax({
+			type: "POST",
+			url: "AjaxOperation.php",
+			data: {
+				catname: cname,
+				method: 'insertcat'
+			},
+			success: function(data) {
+				alert(data);
+			}
+		})
+	}
+</script>
+<?php include_once("footer.php") ?>
--- LOOK_Online_Shop/adminPanel/AddSubCategory.php
+++ LOOK_Online_Shop/adminPanel/AddSubCategory.php
@@ -1,73 +1,51 @@
 <?php include_once("navbar.php");
-$sql="SELECT *FROM category";
-$result=$con->query($sql);
+$sql = "SELECT *FROM category";
+$result = $con->query($sql);
 ?>
-	
-
 <div id="page-wrapper">
-<div class="container">
-	<div></div>
-		<div class="form-grids row widget-shadow" data-example-id="basic-forms"> 
-						<div class="form-title">
-							<h4>Add Sub Category :</h4>
-						</div>
-						<div class="form-body">
-						
-							
-							<form>
-													
-														<div class="form-group">
-														<label>Enter The Sub Category Name</label>
-														<input type="text" name="sub_cat_name" style="width: 50%;" class="form-control" id="sub_cat_name"/>
-														</div>
-														
-								<div class="form-group">
-														<label>Select the Category Name</label>
-															<select id="cat" class="form-control" style="width: 50%;">
-																<?php foreach($result as $row){?>
-																	<option value="<?php echo $row['ID']?>"><?php echo $row['NAME']?></option>
-																	
-																<?php }?>
-															</select>
-														</div>
-								
-								
-								
-									<button onClick="Insert();" class="btn btn-success">Insert</button>
-																		
-													</form>
-							
-						</div>
+	<div class="container">
+		<div></div>
+		<div class="form-grids row widget-shadow" data-example-id="basic-forms">
+			<div class="form-title">
+				<h4>Add Sub Category :</h4>
+			</div>
+			<div class="form-body">
+				<form>
+					<div class="form-group">
+						<label>Enter The Sub Category Name</label>
+						<input type="text" name="sub_cat_name" style="width: 50%;" class="form-control" id="sub_cat_name" />
 					</div>
-	
-	
-	
-</div>
-</div>
+					<div class="form-group">
+						<label>Select the Category Name</label>
+						<select id="cat" class="form-control" style="width: 50%;">
+							<?php foreach ($result as $row) { ?>
+								<option value="<?php echo $row['ID'] ?>"><?php echo $row['NAME'] ?></option>
 
+							<?php } ?>
+						</select>
+					</div>
+					<button onClick="Insert();" class="btn btn-success">Insert</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
 <script>
-		   							function Insert(){
-										
-										var scname=$("#sub_cat_name").val();
-										var dropdownid=$("#cat").val();
-										
-										
-										$.ajax({
-											
-											type:"POST",
-											url:"AjaxOperation.php",
-											data:{subcatname:scname,catid:dropdownid,method:'insertsubcat'},
-											success:function(data){
-												alert(data);
-											}
-											
-											
-										})
-										
-										
-									}
-		   
-		   
-		   					</script>
-<?php include_once("footer.php")?>
-
+	function Insert() {
+		var scname = $("#sub_cat_name").val();
+		var dropdownid = $("#cat").val();
+		$.ajax({
+			type: "POST",
+			url: "AjaxOperation.php",
+			data: {
+				subcatname: scname,
+				catid: dropdownid,
+				method: 'insertsubcat'
+			},
+			success: function(data) {
+				alert(data);
+			}
+		})
+	}
+</script>
+<?php include_once("footer.php") ?>
